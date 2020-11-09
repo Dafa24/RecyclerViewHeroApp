6352dd1ce727e6e7d57c51fe31d450f838c67232
Dafa24 <akbarpradipa@gmail.com>
2020-11-09 10:30:01 +0700

    First Commit?

---
+++ app/src/main/java/com/example/recyclerviewapp/CardViewAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.myrecyclerview
+
+import Hero
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.recyclerviewapp.R
+import kotlinx.android.synthetic.main.item_cardview_hero.view.*
+
+class CardViewHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<CardViewHeroAdapter.CardViewViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): CardViewViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_cardview_hero, viewGroup, false)
+        return CardViewViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CardViewViewHolder, position: Int) {
+        holder.bind(listHero[position])
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    inner class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(hero: Hero) {
+            with(itemView) {
+                Glide.with(itemView.context)
+                    .load(hero.photo)
+                    .apply(RequestOptions().override(350, 550))
+                    .into(img_item_photo)
+                tv_item_name.text = hero.name
+                tv_item_description.text = hero.description
+                btn_set_favorite.setOnClickListener {
+                    Toast.makeText(itemView.context, "Kamu telah menambahkan ${hero.name} ke Favorite", Toast.LENGTH_SHORT).show()
+                }
+                btn_set_share.setOnClickListener {
+                    Toast.makeText(itemView.context, "${hero.name} Telah kamu share", Toast.LENGTH_SHORT).show()
+                }
+                itemView.setOnClickListener { Toast.makeText(itemView.context, "Kamu telah memilih ${hero.name}", Toast.LENGTH_SHORT).show() }
+            }
+            itemView.setOnClickListener { Toast.makeText(itemView.context, "Kamu telah memilih ${hero.name}", Toast.LENGTH_SHORT).show() }
+        }
+    }
+}
